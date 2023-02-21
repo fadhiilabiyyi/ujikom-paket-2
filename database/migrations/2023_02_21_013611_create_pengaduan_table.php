4a7a968063f160dc11951847402470916f8d9a62
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-21 09:00:57 +0700

    Create migration files

---
+++ database/migrations/2023_02_21_013611_create_pengaduan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->integer('id_pengaduan')->autoIncrement();
+            $table->date('tanggal_pengaduan');
+            $table->char('nik');
+            $table->foreign('nik')->references('nik')->on('masyarakat')->onUpdate('cascade')->onDelete('cascade');
+            $table->longText('isi_laporan');
+            $table->string('foto');
+            $table->enum('status', ['0', 'proses', 'selesai']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+};
---
+++ database/migrations/2023_02_21_013830_create_tanggapan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tanggapan', function (Blueprint $table) {
+            $table->integer('id_tanggapan')->autoIncrement();
+            $table->integer('id_pengaduan');
+            $table->foreign('id_pengaduan')->references('id_pengaduan')->on('pengaduan')->onUpdate('cascade')->onDelete('cascade');
+            $table->date('tanggal_tanggapan');
+            $table->longText('tanggapan');
+            $table->integer('id_petugas');
+            $table->foreign('id_petugas')->references('id_petugas')->on('petugas')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tanggapan');
+    }
+};
