9c2a0d9929fa5b27730aa05787a2279073ff1eb4
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-23 14:27:22 +0700

    Web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PengaduanController;
+use App\Http\Controllers\MasyarakatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +17,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::resource('/pengaduan', PengaduanController::class);
+
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest:petugas,masyarakat');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth:petugas,masyarakat');
+
+Route::get('/form', function () {
+    return view('masyarakat.form');
+})->middleware(['auth:petugas,masyarakat', 'checkGuard:masyarakat']);
+
+Route::get('/register', [MasyarakatController::class, 'index'])->name('register');
+Route::post('/register', [MasyarakatController::class, 'store'])->name('register-post');
